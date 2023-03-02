10e7f9d8b2cb0c54039bd8f8ee9652331705aed0
Stern <70122891+SternXD@users.noreply.github.com>
2023-03-02 13:40:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ def calculate_area(radius):
     area = math.pi * radius ** 2
     return area
 
+# Define a function that takes square meters as an argument and returns acres
+def convert_to_acres(square_meters):
+    # Use the formula acres = square meters x 0.000247
+    acres = square_meters * 0.000247
+    return acres
+
 # Ask the user for input radius
 radius = float(input("Enter the radius: "))
 
-# Call the function and print the result
+# Call the functions and print the result
 area = calculate_area(radius)
-print(f"The area of a circle with radius {radius} is {area} square meters.")
+acres = convert_to_acres(area)
+print(f"The area of a circle with radius {radius} is {area} square meters or {acres} acres.")
